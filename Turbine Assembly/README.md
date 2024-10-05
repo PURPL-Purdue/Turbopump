887084283f2e61733d1ac802816c81c25b5a7a38
Amanjyoti Mridha <71572787+QuackingBob@users.noreply.github.com>
2024-10-05 19:35:08 -0400

    Update Repo Structure and Add Parameter Loading File

--- Turbine Assembly/README.md
+++ Turbine Assembly/README.md
@@ -5,3 +5,7 @@
 
 ## Rotor & Stator
 [Put desgn requirements here]
+
+## How to load constants:
+1. Run `main`
+2. call `load('params.mat')` to load constants into your current workspace (in the code)
--- Turbine Assembly/main.m
+++ Turbine Assembly/main.m
@@ -1,1 +1,19 @@
 %% THIS SCRIPT LOADS ALL DESIGN CONSTANTS INTO A FILE CALLED "params.mat"
+params_file = 'params.mat';
+
+%% Turbine Constants
+% TODO: Add final turbopump constants
+
+%% Cold Gas Testing Constants:
+rotor_radius = 0.04; % [m]
+mass_flow_n2 = 0.17; % [kg/s]
+shaft_power = 10; % [kW]
+turbine_rpm = 10000; % [rpm]
+input_velocity = NaN; % TODO: From Andrew
+nozzle_radius = NaN; % TODO: From Andrew
+% TODO: Andrew's constants for nozzle:
+
+
+%% Save Parameters
+save(params_file, ...
+    "rotor_radius", "input_velocity", "turbine_rpm", "shaft_power", "mass_flow_n2");
