2979bac1dcfefb66244004b0552d2453b09bc3da
ChrisSuffredini <bobchris8300@gmail.com>
2025-02-01 15:31:40 -0500

    Update IPS_leakage_calcs.m

--- Pump Assembly/Calculations/IPS_leakage_calcs.m
+++ Pump Assembly/Calculations/IPS_leakage_calcs.m
@@ -1,37 +1,37 @@
 %input
 format long;
-seal_radii_min = .257; %inches
-step = .02; %inches
+seal_radii_min = .315; %inches
+step = .011; %inches
 sealNum = 6; %number of seals in the seal stack up
 
-clearance = .002; %inches
+clearance = .0025; %inches
 tooth_width = .01; %inches
 cavity_width = .031; %inches
+
 teeth = 5;
+
 loxPin = 1000; %psi
-loxPdrain = 50; %psi
-loxPvent = 1000; %psi
-loxRho = 1140; %kg/m^3
+loxRecycleP = 50; %psi
+loxRho = 1140; % density kg/m^3
 loxMu = 1.95e-4; %Pa s
 
 atmP = 14.7; %psi
+N2inPressure = 1000; %psi
 
-rp1Pin = 1000; %psi
-rp1Pdrain = 60; %psi
-rp1Pvent = 1000; %psi
-rp1Rho = 800; %kg/m^3
-rp1Mu = 1.92e-3; %Pa s
+fuelPin = 1000; %psi
+fuelRecycleP = 60; %psi
+fuelRho = 800; %kg/m^3
+fuelMu = 1.92e-3; %Pa s
 
 
 
 
 %change units
 loxPin = 6894.76*loxPin;
-loxPdrain = 6894.76*loxPdrain;
-loxPvent = 6894.76*loxPvent;
-rp1Pin = 6894.76*rp1Pin;
-rp1Pdrain = 6894.76*rp1Pdrain;
-rp1Pvent = 6894.76*rp1Pvent;
+loxRecycleP = 6894.76*loxRecycleP;
+N2inPressure = 6894.76*N2inPressure;
+fuelPin = 6894.76*fuelPin;
+fuelRecycleP = 6894.76*fuelRecycleP;
 atmP = atmP * 6894.76;
 
 step = step * .0245;
@@ -46,24 +46,27 @@ for i=1:sealNum
     seal_radii(i) = seal_radii_min + (i-1)*step;
 end
 
-[mdotlox1,Relox1] = laby_leakage(clearance, tooth_pitch, tooth_width, seal_radii(1), teeth, loxPin, loxPdrain, loxRho, loxMu); %kg/s
-Qlox1 = mdotlox1 / loxRho;
-mdotlox1 = mdotlox1*2.2046 %lbm/s
-Qlox1 = Qlox1*264.171; %gal/s;
+[mdotloxRecyle,ReloxRecycle] = laby_leakage(clearance, tooth_pitch, tooth_width, seal_radii(1), teeth, loxPin, loxRecycleP, loxRho, loxMu); %kg/s
+QloxRecycle = mdotloxRecyle / loxRho;
+mdotloxRecyle = mdotloxRecyle*2.2046; %lbm/s
+QloxRecycle = QloxRecycle*264.171; %gal/s;
 
-[mdotlox2, Relox2] = laby_leakage(clearance, tooth_pitch, tooth_width, seal_radii(2), teeth, loxPdrain, atmP, loxRho, loxMu);
-Qlox2 = mdotlox2 / loxRho;
-mdotlox2 = mdotlox2*2.2046 %lbm/s
-Qlox2 = Qlox2*264.1721; %gal/s
+[mdotLoxDrain, ReLoxDrain] = laby_leakage(clearance, tooth_pitch, tooth_width, seal_radii(2), teeth, loxRecycleP, atmP, loxRho, loxMu);
+QloxDrain = mdotLoxDrain / loxRho;
+mdotLoxDrain = mdotLoxDrain*2.2046; %lbm/s
+QloxDrain = QloxDrain*264.1721; %gal/s
 
-[mdotRp11,ReRp1] = laby_leakage(clearance, tooth_pitch, tooth_width, seal_radii(6), teeth, rp1Pin, rp1Pdrain, rp1Rho, rp1Mu); %kg/s
-Qrp11 = mdotRp11 / rp1Rho *264.172;%gal/s
-mdotRp11 = mdotRp11 * 2.2046 %lbm/s
+[mdotfuelRecycle,ReFuelRecycle] = laby_leakage(clearance, tooth_pitch, tooth_width, seal_radii(6), teeth, fuelPin, fuelRecycleP, fuelRho, fuelMu); %kg/s
+QfuelRecycle = mdotfuelRecycle / fuelRho *264.172; %gal/s
+mdotfuelRecycle = mdotfuelRecycle * 2.2046; %lbm/s
 
 
 
-[mdotRp12, ReRp2] = laby_leakage(clearance, tooth_pitch, tooth_width, seal_radii(5), teeth, rp1Pdrain, atmP, rp1Rho, rp1Mu); %kg/s
-Qrp11 = mdotRp12 /rp1Rho * 264.172;%gal/s
-mdotRp12 = mdotRp12 * 2.2046   %lbm/s
+[mdotfuelDrain, ReFuelDrain] = laby_leakage(clearance, tooth_pitch, tooth_width, seal_radii(5), teeth, fuelRecycleP, atmP, fuelRho, fuelMu); %kg/s
+QfuelDrain = mdotfuelDrain /fuelRho * 264.172;   %gal/s
+mdotfuelDrain = mdotfuelDrain * 2.2046;   %lbm/s
 
-fprintf("%f, %f, %f, %f",Relox1, Relox2, ReRp1, ReRp2)
+
+fprintf("\n\nReynolds Numbers:\nLOx Recycle = %f\nLox Drain = %f\nFuel Recycle = %f\nFuel Drain = %f\n",ReloxRecycle, ReLoxDrain, ReFuelRecycle, ReFuelDrain)
+fprintf("\nLeakage Rates (lbm/s):\n LOx Recycle = %f\nLox Drain = %f\nFuel Recycle = %f\nFuel Drain = %f\n",mdotloxRecyle, mdotLoxDrain, mdotfuelRecycle, mdotfuelDrain)
+fprintf("\nLeakage Rates (gal/s):\n LOx Recycle = %f\nLox Drain = %f\nFuel Recycle = %f\nFuel Drain = %f\n",QloxRecycle, QloxDrain, QfuelRecycle, QfuelDrain)
