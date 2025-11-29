8594b7d48c08983baeb1bef8e4b18f220fd66e15
devinmorris15 <devinmorrisjr06@gmail.com>
2025-11-29 16:46:30 -0700

    updating graph

--- TCA/TCA Sizing Code Files/Final_Contouring_Script.py
+++ TCA/TCA Sizing Code Files/Final_Contouring_Script.py
@@ -436,17 +436,25 @@ def plot_nozzle_inches(contour, dia_t, dia_c, dia_e, len_c):
 	plt.annotate( "", [xbell[-1], 0.95 * nybell[-1]], [xbell[-1], 0.95 * ybell[-1]], arrowprops=dict(lw=1, arrowstyle='|-|') )
 	plt.text(4.0,0.1, text, fontsize=25 )	
 
+	con_len = len_c - abs(xecc[0])
+
 	# chamber length line
-	text = str(round(len_c,2)) + 'in'
+	text = str(round(con_len,2)) + 'in'
 	# draw dimension from [0,0] to [xe[-1], ye[-1]]
-	plt.annotate( "", [xecc[-1], 1.2 * ybell[-1]], [0, 1.2 * ybell[-1]], arrowprops=dict(lw=1, arrowstyle='|-|') )
-	plt.text((-len_c / 2), 1.25* ybell[-1], text, fontsize=25 )
+	plt.annotate( "", [(xecc[-1] - 0.05), 1.2 * ybell[-1]], [(xecc[0] + 0.05), 1.2 * ybell[-1]], arrowprops=dict(lw=1, arrowstyle='|-|') )
+	plt.text(-8.9, 1.25* ybell[-1], text, fontsize=25 )
+
+	# convergent length line
+	text = str(round(abs(xecc[0]),2)) + 'in'
+	# draw dimension from [0,0] to [xe[-1], ye[-1]]
+	plt.annotate( "", [(xecc[0] - 0.05), 1.2 * ybell[-1]], [0.05, 1.2 * ybell[-1]], arrowprops=dict(lw=1, arrowstyle='|-|') )
+	plt.text(-2.0, 1.25* ybell[-1], text, fontsize=25 )
 
 	# divergent section length line
 	text = str(round(xbell[-1],2)) + 'in'
 	# draw dimension from [0,0] to [xe[-1], ye[-1]]
-	plt.annotate( "", [0, 1.2 * ybell[-1]], [xbell[-1], 1.2 * ybell[-1]], arrowprops=dict(lw=1, arrowstyle='|-|') )
-	plt.text((xbell[-1] / 2), 1.25* ybell[-1], text, fontsize=25 )
+	plt.annotate( "", [-0.05, 1.2 * ybell[-1]], [(xbell[-1] + 0.05), 1.2 * ybell[-1]], arrowprops=dict(lw=1, arrowstyle='|-|') )
+	plt.text(2.0, 1.25* ybell[-1], text, fontsize=25 )
 
 	# axis
 	plt.axhline(color='black', lw=0.5, linestyle="dashed")
