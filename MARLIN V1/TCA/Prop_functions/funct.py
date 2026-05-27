0486d85406d9038da648bc7a70a1f288a737cd3f
Lysenko69 <121409082+Lysenko69@users.noreply.github.com>
2026-05-27 23:07:59 +0200

    pp

--- MARLIN V1/TCA/Prop_functions/funct.py
+++ MARLIN V1/TCA/Prop_functions/funct.py
@@ -1,27 +1,48 @@
-from rocketcea.cea_obj import CEA_Obj
-import matplotlib.pyplot as plt
+from rocketcea.cea_obj_w_units import CEA_Obj
 import numpy as np
 
-def get_isp_heatmap(Fuel,Ox, mr_range, p_range, pamb):    
-    CEA = CEA_Obj(oxName=Ox, fuelName=Fuel)
+
+def _mixture_density(mr, rho_fuel, rho_ox):
+    return mr / (mr + 1.0) * rho_ox + 1.0 / (mr + 1.0) * rho_fuel
+
+def get_isp_heatmap(Fuel, Ox, mr_range, p_range, pamb, rho_fuel, rho_ox):
+    CEA = CEA_Obj(
+        oxName=Ox,
+        fuelName=Fuel,
+        pressure_units="bar",
+        temperature_units="K",
+        cstar_units="m/s",
+        sonic_velocity_units="m/s",
+        density_units="kg/m^3",
+        isp_units="sec",
+    )
     Mr = np.arange(mr_range[0], mr_range[1], 0.1)
     Pin = np.arange(p_range[0], p_range[1], 1)
     isp_grid = np.zeros((len(Mr), len(Pin)))
+    density = np.zeros(len(Mr))
 
-    for i in range(len(Mr)):
-        for j in range(len(Pin)):
-            mr = Mr[i]
-            pc = Pin[j]
+    for i, mr in enumerate(Mr):
+        density[i] = _mixture_density(mr, rho_fuel, rho_ox)
+        for j, pc in enumerate(Pin):
             Eps = CEA.get_eps_at_PcOvPe(Pc=pc, MR=mr, PcOvPe=(pc / pamb))
             isp = CEA.estimate_Ambient_Isp(Pc=pc, MR=mr, eps=Eps, Pamb=pamb)
             mr_stoich = CEA.getMRforER(ERphi=1.0)
             isp_grid[i, j] = isp[0]
 
-    return Mr, Pin, isp_grid, mr_stoich
+    return Mr, Pin, isp_grid, mr_stoich,density
 
-def get_tempstar_heatmap(Fuel,Ox, mr_range, p_range, pamb):  
-    rankineToKelvin = 5.0 / 9.0  
-    CEA = CEA_Obj(oxName=Ox, fuelName=Fuel)
+
+def get_tempstar_heatmap(Fuel, Ox, mr_range, p_range, pamb):
+    CEA = CEA_Obj(
+        oxName=Ox,
+        fuelName=Fuel,
+        pressure_units="bar",
+        temperature_units="K",
+        cstar_units="m/s",
+        sonic_velocity_units="m/s",
+        density_units="kg/m^3",
+        isp_units="sec",
+    )
     Mr = np.arange(mr_range[0], mr_range[1], 0.1)
     Pin = np.arange(p_range[0], p_range[1], 1)
     tempstar_grid = np.zeros((len(Mr), len(Pin)))
@@ -31,8 +52,8 @@ def get_tempstar_heatmap(Fuel,Ox, mr_range, p_range, pamb):
             mr = Mr[i]
             pc = Pin[j]
             Eps = CEA.get_eps_at_PcOvPe(Pc=pc, MR=mr, PcOvPe=(pc / pamb))
-            tempstar = CEA.get_Temperatures(Pc=pc, MR=mr, eps=Eps) 
+            tempstar = CEA.get_Temperatures(Pc=pc, MR=mr, eps=Eps)
             mr_stoich = CEA.getMRforER(ERphi=1.0)
-            tempstar_grid[i, j] = tempstar[0] * rankineToKelvin
+            tempstar_grid[i, j] = tempstar[1]
 
     return Mr, Pin, tempstar_grid, mr_stoich
--- MARLIN V1/TCA/Prop_functions/propellants.py
+++ MARLIN V1/TCA/Prop_functions/propellants.py
@@ -1,11 +1,10 @@
-from rocketcea.cea_obj import CEA_Obj
 from rocketcea.cea_obj import add_new_fuel
 import matplotlib.pyplot as plt
-import numpy as np
-from funct import get_isp_heatmap 
-from funct import get_tempstar_heatmap 
+from funct import get_isp_heatmap
+from funct import get_tempstar_heatmap
 
-## To add acetone - https://atct.anl.gov/Thermochemical%20Data/version%201.130/species/?species_number=54 for Enthalpy and https://pubchem.ncbi.nlm.nih.gov/compound/Acetone for density
+
+##### To add acetone - https://atct.anl.gov/Thermochemical%20Data/version%201.130/species/?species_number=54 for Enthalpy and https://pubchem.ncbi.nlm.nih.gov/compound/Acetone for density
 acetone_card = """
 fuel Acetone  C 3 H 6 O 1
 h,kj/mol=-216.94
@@ -13,35 +12,53 @@ t(k)=298.15
 rho=0.79
 """
 add_new_fuel('Acetone', acetone_card)
-
 #####
 
-Fuel = 'Acetone'
+Fuel = 'JetA' 
+rho_fuel = 790  # kg/m^3 (0.79 g/cm^3 acetone density)
+rho_ox = 1141  # kg/m^3 (LOX density)
 Ox = 'LOX'
 mr_range = (0.55, 6.0)
-p_range = (15, 60)
-pamb = 1.01325
+p_range = (15, 60)  # bar
+pamb = 1.01325  # bar
 
-Mr, Pin, isp_grid, mr_stoich = get_isp_heatmap(Fuel=Fuel, Ox=Ox, mr_range=mr_range, p_range=p_range, pamb=pamb)
-Mr, Pin, tempstar_grid, mr_stoich = get_tempstar_heatmap(Fuel=Fuel, Ox=Ox, mr_range=mr_range, p_range=p_range, pamb=pamb)
-
-fig, axes = plt.subplots(1, 2, figsize=(16, 6))
-mesh1 = axes[0].pcolormesh(Pin,Mr,isp_grid,shading='auto',cmap='viridis')
-axes[0].axhline(mr_stoich,color='red',linestyle='-',linewidth=1,label=f'Stoich O/F = {mr_stoich:.2f}')
+Mr, Pin, isp_grid, mr_stoich, density = get_isp_heatmap(
+    Fuel=Fuel,
+    Ox=Ox,
+    mr_range=mr_range,
+    p_range=p_range,
+    pamb=pamb,
+    rho_fuel=rho_fuel,
+    rho_ox=rho_ox,
+)
+Mr, Pin, tempstar_grid, mr_stoich = get_tempstar_heatmap(
+    Fuel=Fuel,
+    Ox=Ox,
+    mr_range=mr_range,
+    p_range=p_range,
+    pamb=pamb,
+)
+fig, axes = plt.subplots(1, 3, figsize=(24, 6))
+mesh1 = axes[0].pcolormesh(Pin, Mr, isp_grid, shading='auto', cmap='viridis')
+axes[0].axhline(mr_stoich, color='red', linestyle='-', linewidth=1, label=f'Stoich O/F = {mr_stoich:.2f}')
 fig.colorbar(mesh1, ax=axes[0], label='Ambient Isp (s)')
 axes[0].set_xlabel('Chamber Pressure (bar)')
 axes[0].set_ylabel('O/F Ratio')
 axes[0].set_title(f'Specific Impulse Heatmap ({Ox} / {Fuel})')
 axes[0].legend()
 
-
-mesh2 = axes[1].pcolormesh(Pin,Mr,tempstar_grid,shading='auto',cmap='viridis')
-axes[1].axhline(mr_stoich,color='red',linestyle='-',linewidth=1,label=f'Stoich O/F = {mr_stoich:.2f}')
+mesh2 = axes[1].pcolormesh(Pin, Mr, tempstar_grid, shading='auto', cmap='viridis')
+axes[1].axhline(mr_stoich, color='red', linestyle='-', linewidth=1, label=f'Stoich O/F = {mr_stoich:.2f}')
 fig.colorbar(mesh2, ax=axes[1], label='Temperature Star (K)')
 axes[1].set_xlabel('Chamber Pressure (bar)')
 axes[1].set_ylabel('O/F Ratio')
 axes[1].set_title(f'Throat Temperature Heatmap ({Ox} / {Fuel})')
 axes[1].legend()
+
+mesh3 = axes[2].pcolormesh(Pin, density, isp_grid, shading='auto', cmap='viridis')
+axes[2].set_xlabel('Chamber Pressure (bar)')
+axes[2].set_ylabel('Density (kg/m^3)')
+axes[2].set_title(f'Isp Heatmap ({Ox} / {Fuel})')
 
 plt.tight_layout()
 plt.show()
