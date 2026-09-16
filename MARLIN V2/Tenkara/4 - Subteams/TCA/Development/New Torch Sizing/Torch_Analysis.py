92c6ce78e9eb899f2b9adbe381911a144b96257c
OPalmerME <palme185@purdue.edu>
2026-09-15 21:53:28 -0400

    Update Torch_Analysis.py

--- MARLIN V2/Tenkara/4 - Subteams/TCA/Development/New Torch Sizing/Torch_Analysis.py
+++ MARLIN V2/Tenkara/4 - Subteams/TCA/Development/New Torch Sizing/Torch_Analysis.py
@@ -1,15 +1,36 @@
 # Torch_Analysis
-# Torch_Analysis
-from New_TCA_Igniter_Sizing import choked_backpressure
+import numpy as np
+from New_TCA_Igniter_Sizing import diameter_to_area
+from New_TCA_Igniter_Sizing import area_to_diameter
 
-## Define torch parameters
-At = 1     # Throat area
-S_f = 1     # Fuel injector size
-S_o = 1     # Oxidizer injector size
-k = 1       #Specific heat ratio
-p_c = 1     # Torch Chamber Pressure
-stiff = 0.30 # Stiffness of the torch
+R = 8.314 # Gas constant
 
-line_pressure = choked_backpressure(k, stiff, p_c)
+## Fixed torch parameters
+A_t = 1    # Throat area
+Df = 1     # Fuel injector diameter
+Do = 1     # Oxidizer injector diameter
+Tc = 1     # Chamber temperature
+Tox = 1     # Oxidizer line temperature
+Tf = 1      # Fuel line temperature
+k = 1     # gamma
+k_ox = 1      # Oxidizer gamma
+k_fuel = 1     # Fuel gamma
+mdot = 1 # Mass flow rate
+of_ratio = 1 # O/F Ratio
+
+def chamber_pressure(throatArea, k, massflow, chamberTemp):
+#   Input:
+    # Throat area
+    # Heat capacity ratio
+    # Mass flow rate
+    # Chamber temperature
+#   Output:
+    # Chamber Pressure
+    return (massflow/(throatArea*k))*((np.sqrt(k*R*chamberTemp))/(np.sqrt((2/(k+1))**((k+1)/(k-1)))))
 
 
+
+mdot_ox = mdot * (of_ratio)/(of_ratio + 1)
+mdot_f = mdot - mdot_ox
+
+Pc = chamber_pressure(A_t, k, mdot, Tc)
