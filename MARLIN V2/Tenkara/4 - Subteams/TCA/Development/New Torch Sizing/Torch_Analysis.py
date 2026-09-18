f0e4f3978f5b5286374e083134d69f26899d055e
OPalmerME <palme185@purdue.edu>
2026-09-17 21:03:09 -0400

    Update Torch_Analysis.py

--- MARLIN V2/Tenkara/4 - Subteams/TCA/Development/New Torch Sizing/Torch_Analysis.py
+++ MARLIN V2/Tenkara/4 - Subteams/TCA/Development/New Torch Sizing/Torch_Analysis.py
@@ -1,17 +1,20 @@
 # Torch_Analysis
 import numpy as np
+from pyfluids import Fluid, FluidsList, Input
 from New_TCA_Igniter_Sizing import diameter_to_area
 from New_TCA_Igniter_Sizing import area_to_diameter
 
 R = 8.314 # Gas constant
+CM_TO_IN = 1/2.54 # Centimeters to Inches
+IN_TO_CM = 2.54 # Inches to Centimeters 
 
 ## Fixed torch parameters
 A_t = 1    # Throat area
-Df = 1     # Fuel injector diameter
-Dox = 1     # Oxidizer injector diameter
+Df = 1 * IN_TO_CM     # Fuel injector diameter (input in inches)
+Dox = 1 * IN_TO_CM    # Oxidizer injector diameter
 Tc = 1     # Chamber temperature
-Tox = 1     # Oxidizer line temperature
-Tf = 1      # Fuel line temperature
+Tox = 22    # Oxidizer line temperature
+Tf = 22    # Fuel line temperature
 k = 1     # gamma
 k_ox = 1      # Oxidizer gamma
 k_fuel = 1     # Fuel gamma
@@ -36,4 +39,3 @@ mdot_f = mdot - mdot_ox
 Pc = chamber_pressure(A_t, k, mdot, Tc)
 Pox = chamber_pressure(diameter_to_area(Dox), k, mdot_ox, Tox)
 Pf = chamber_pressure(area_to_diameter(Df), k, mdot_f, Tf)
-
